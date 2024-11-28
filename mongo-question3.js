6df7e67acec1b6bba83ee0cee9cb37c0e22bc17c
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2024-11-28 12:21:08 +0530

    solution of mongo question

---
+++ mongo-question3.js
@@ -0,0 +1,47 @@
+db.sales.aggregate([
+    {
+      $unwind: "$items",
+    },
+    {
+      $addFields: {
+        month: {$dateToString: {format: "%Y-%m", date: "$date"}}
+      }
+    },
+    {
+      $addFields: {
+        itemRevenue: { $multiply: ["$items.quantity","$items.price"] }
+      }
+    },
+    {
+      $group: {
+        _id: {store: "$store", month: "$month"},
+        totalRevenue: {
+          $sum: "$itemRevenue"
+        },
+        totalPrice: {
+          $sum: "$items.price"
+        },
+         totalQuantity: { $sum: "$items.quantity" }
+      }
+    },
+    {
+      $addFields: {
+        averagePrice: { $divide: ["$totalPrice", "$totalQuantity"] }
+      }
+    },
+    {
+      $project: {
+        _id:0,
+        store: "$_id.store",
+        month: "$_id.month",
+        totalRevenue: 1,
+        averagePrice: 1
+      }
+    },
+    {
+      $sort: {
+        store: 1, 
+        month: 1
+      }
+    }
+  ])
